b7980aba4c3ddaf7b25efa43c8dccb37d39ad24f
Crivetkaaa <crivetkaa@vk.com>
2023-04-18 03:20:26 +0300

    Доработана диограмма

--- test.py
+++ test.py
@@ -1,18 +1,33 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+
+
 fig = plt. figure()
-ax = fig.add_subplot(1, 1, 1)
 
 xs = []
 ys = []
 color = ['-g']
-    
+average = []
+
+her = {}
+all_values_from_test = []
+vals = []
+labels = []
+color_diogram = []
+
 def animate(i):
-    ax.clear()
-    ax.plot(xs, ys, color[-1])
+    fig.clear()
+    plt.subplot(1,2,1)
+    plt.plot(xs, ys, color[-1])
+    plt.plot(xs, average, '-.r')
     plt.xlabel('Номер итерации')
     plt.ylabel('Значение')
     plt.title("График отношения номера итерации к результату")
+    
+    plt.subplot(1, 2, 2)
+    
 
+    plt.pie(vals, labels=labels)   
 ani = animation.FuncAnimation(fig, animate, interval=100)
 plt.show()
